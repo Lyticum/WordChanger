efbaf25a7782957a6bd5b67aa4503f9d5f1586ee
Ozan MERT <ozn.merts@gmail.com>
2023-11-07 22:32:20 +0300

    Add files via upload

---
+++ replace_word.py
@@ -0,0 +1,8 @@
+def replace_word():
+    
+    str= "Hello from the other side"
+    word1 = input("Değiştirmek istediğiniz kelimeyi girin; ")
+    word2 = input("Değiştirilmesini istediğiniz kelimeyi girin; ")
+    print (str.replace(word1,word2))
+
+replace_word()
